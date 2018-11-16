4d3e3fd706df3aa35ab44120119c5496761ccae4
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-16 20:18:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+end
+
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+  end
 end
